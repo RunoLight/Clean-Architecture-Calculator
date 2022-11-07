f9de1e42ba06745b559cc2706bf47b362d056d97
Vasily Kuznetsov <31756050+RunoLight@users.noreply.github.com>
2022-11-07 16:01:46 +1000

    Mock classes for testing

---
+++ Calculator/Assets/Scripts/Calculator/Presenter/Concrete/CountViewMock.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Calculator.Presenter.Concrete
+{
+    public class CountViewMock : ICalculatorView
+    {
+        private readonly GameObject _gameObject = new();
+
+        public Button Button =>
+            _gameObject.GetComponent<Button>() ?? _gameObject.AddComponent<Button>();
+
+        public TMP_InputField InputField =>
+            _gameObject.GetComponent<TMP_InputField>() ?? _gameObject.AddComponent<TMP_InputField>();
+    }
+}
---
+++ Calculator/Assets/Scripts/Calculator/Repository/DataStore/CalculatorDataStoreMock.cs
@@ -0,0 +1,20 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace Calculator.Repository.DataStore
+{
+    public class CalculatorDataStoreMock : ICalculatorDataStore
+    {
+        private string _state = "";
+
+        public UniTask<string> LoadState()
+        {
+            return UniTask.FromResult(_state);
+        }
+
+        public UniTask SaveState(string state)
+        {
+            _state = state;
+            return UniTask.CompletedTask;
+        }
+    }
+}
